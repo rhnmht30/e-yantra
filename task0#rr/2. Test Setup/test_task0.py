905e9485e8df8a7313bf923f3e3313d99604a2bb
Rohan <rhnmht30@gmail.com>
2019-11-03 15:59:12 +0530

    add task 0

---
+++ task0#rr/2. Test Setup/test_task0.py
@@ -0,0 +1,36 @@
+
+'''
+*****************************************************************************************
+*
+*        =============================================
+*           Rapid Rescuer (RR) Theme (eYRC 2019-20)
+*        =============================================
+*
+*  This script is intended to check the versions of the installed
+*  software/libraries in Task 0 of Rapid Rescuer (RR) Theme (eYRC 2019-20).
+*
+*  Filename:			test_task0.py
+*  Created:				20/08/2019
+*  Last Modified:		05/09/2019
+*  Author:				e-Yantra Team
+*  
+*  This software is made available on an "AS IS WHERE IS BASIS".
+*  Licensee/end user indemnifies and will keep e-Yantra indemnified from
+*  any and all claim(s) that emanate from the use of the Software or 
+*  breach of the terms of this agreement.
+*  
+*  e-Yantra - An MHRD project under National Mission on Education using ICT (NMEICT)
+*
+*****************************************************************************************
+'''
+
+
+# Import test module
+import task0_cardinal
+
+
+# Main function
+if __name__ == '__main__':
+
+	task0_cardinal.main()
+
